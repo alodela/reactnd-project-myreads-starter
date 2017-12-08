d6eed0ba3f671ea679bc365186c69b672938e9e3
Alberto Lopez de Lara <alberto.lopez@move.com>
2017-12-07 21:20:11 -0800

    feat: Get books from API and display shelves

---
+++ src/Shelf.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Book from './Book'
+
+class Shelf extends Component {
+
+  render() {
+    const { books, title } = this.props
+
+    console.log(this.props.books)
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li key={book.id}><Book book={book} /></li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Shelf
